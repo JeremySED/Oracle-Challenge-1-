3be7c7e07fd961779b61134d3f815020e0337706
JeremySED <jimicito.1234567@gmail.com>
2025-08-20 13:44:06 -0500

    Ya se puede escribir nombres

---
+++ app.js
@@ -0,0 +1,11 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Debes escribir un nombre.");
+        return;
+    }
+}
